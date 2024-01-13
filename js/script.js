ab21a30ff8567c96c6d808a383c6f00b884f9ad5
anjagutmann <anja.gutmann@hfg-gmuend.de>
2024-01-14 00:05:41 +0100

    update noise-mode

--- js/script.js
+++ js/script.js
@@ -1,59 +1,15 @@
-// Zustand des Noise-Modus
-var isNoiseModeActive = false;
-
-// Referenz zum Body-Element und zum potenziellen Border-Element
-var body = document.body;
-var border = document.createElement('div');
-border.className = 'viewport-border';
-body.appendChild(border);
-
-function simulateDataPoisoning() {
-    // Verhindert, dass die Animation abgespielt wird, wenn der Noise-Modus aktiv ist
-    if (isNoiseModeActive) return;
-  
-    // Kreis-Animation
-    var circle = document.getElementById('growing-circle');
-    var scale = Math.sqrt(window.innerWidth * window.innerWidth + window.innerHeight * window.innerHeight) / 30;
-    circle.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
-    circle.style.opacity = '1';
-  
-    // Beginnt den Prozess, den Kreis zu verbergen, ohne zu schrumpfen
-    setTimeout(function() {
-      circle.style.opacity = '0'; // Kreis wird transparent
-      setTimeout(function() {
-        circle.style.transform = 'translate(-50%, -50%) scale(0)'; // Setzt den Kreis zurück, ohne sichtbare Animation
-      }, 1500); // Wartet auf das Ende der Opazitätstransition
-    }, 1500); // Wartezeit, bis die Opazitätstransition beginnt
-  }
-  
-
-function toggleNoiseMode() {
-  if (isNoiseModeActive) {
-    // Schalte den Noise-Modus aus
-    border.style.display = 'none';
-  } else {
-    // Schalte den Noise-Modus ein
-    border.style.display = 'block';
-  }
-  isNoiseModeActive = !isNoiseModeActive;
+// JavaScript
+function onNoiseButtonClick() {
+  // Hier könnten Sie weitere Aktionen hinzufügen, falls nötig
+  document.body.classList.toggle('noise-active');
 }
 
-// function onNoiseButtonClick() {
-//     // Annahme, dass 'top-bar' die Klasse ist, die Ihre Topbar identifiziert
-//     const topBar = document.querySelector('.top-bar');
-//     topBar.classList.toggle('animate-line');
-// }
+// JavaScript
+function toggleNoiseMode() {
+  isNoiseModeActive = !isNoiseModeActive;
+  document.body.classList.toggle('noise-active', isNoiseModeActive);
+}
 
-/// Entfernen Sie die erste Definition von onNoiseButtonClick
-function onNoiseButtonClick() {
-    simulateDataPoisoning();
-    toggleNoiseMode();
-    
-    // Toggle der Klasse für die Animation der Linie
-    const topBar = document.querySelector('.top-bar');
-    topBar.classList.toggle('animate-line');
-  }
-  
 
 // Funktion zum Hinzufügen von Klick-Event-Listeners zu jedem Tab
 function setupTabs() {
